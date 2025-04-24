af96fa16a4a69b229c36c4daf2e1d552a40977d2
Hamsa <hamsam0097@gmail.com>
2025-04-25 01:13:56 +0300

    final commit since this project wont be on portfolio

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const myLibrary = [
       publishDate: "180",
       coverArt: "https://covers.openlibrary.org/b/isbn/9780140449334-L.jpg",
       dateAdded: 1713800000000,
-      review: null
     },
     {
       id: "2b3c4d5e-1111-2222-3333-bbcdefbbcdef",
@@ -17,7 +16,6 @@ const myLibrary = [
       publishDate: "2005",
       coverArt: "https://covers.openlibrary.org/b/isbn/9780553582024-L.jpg",
       dateAdded: 1713800000100,
-      review: null
     },
     {
       id: "3c4d5e6f-1111-2222-3333-ccdefccdefcc",
@@ -27,7 +25,6 @@ const myLibrary = [
       publishDate: "2007",
       coverArt: "https://covers.openlibrary.org/b/isbn/9780756404741-L.jpg",
       dateAdded: 1713800000200,
-      review: null
     },
     {
       id: "4d5e6f70-1111-2222-3333-dddefdddefdd",
@@ -37,7 +34,6 @@ const myLibrary = [
       publishDate: "2012",
       coverArt: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1707559940i/44777327.jpg",
       dateAdded: 1713800000300,
-      review: null
     },
     {
       id: "5e6f7081-1111-2222-3333-eeefeeefeeee",
@@ -47,7 +43,6 @@ const myLibrary = [
       publishDate: "1995",
       coverArt: "https://covers.openlibrary.org/b/isbn/9780553573398-L.jpg",
       dateAdded: 1713800000400,
-      review: null
     },
     {
       id: "6f708192-1111-2222-3333-fff0fff0ffff",
@@ -57,14 +52,16 @@ const myLibrary = [
       publishDate: "2014",
       coverArt: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1373039517i/17910048.jpg",
       dateAdded: 1713800000500,
-      review: null
     }
   ];
 
 const mainDiv = document.querySelector('main');
 
 
-function Book(title, author, pages, publishDate, coverArt = null, rating = null, read = false) {
+function Book(title, author, pages, publishDate, coverArt = null) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
@@ -72,8 +69,6 @@ function Book(title, author, pages, publishDate, coverArt = null, rating = null,
   this.publishDate = publishDate;
   this.coverArt = coverArt;
   this.dateAdded = Date.now();
-  this.rating = rating;
-  this.read = read;
 }
 
 function addBookToLibrary(book) {
@@ -86,7 +81,6 @@ function displayBook(book){
     card.classList.add('card');
     for (let key in book){
         if (key === 'author' || key === 'title' || key === 'pages' || key === 'publishDate'){
-            console.log(key)
             let p = document.createElement('p');
             let text;
             if (key === 'pages'){
@@ -111,18 +105,19 @@ function displayBook(book){
     const bookCoverContainer = document.createElement('div');
     bookCoverContainer.classList.add('coverArtContainer');
     const bookCover = document.createElement('img');
+    console.log(book.coverArt);
     bookCover.classList.add('coverArt')
     book.coverArt ? bookCover.src = book.coverArt : bookCover.style.backgroundColor = 'green';
     card.append(bookCoverContainer);
-    bookCoverContainer.append(bookCover)
+    bookCoverContainer.append(bookCover);
 
-    const starDiv = document.createElement('div');
-    let star = document.createElement('span');
-    star.classList.add(['fa', 'fa-star']);
-    for (let i =0; i<5 ; i++){
-        starDiv.append(star)
-    }
-    card.append(starDiv);
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete');
+    deleteButton.textContent = 'X';
+    deleteButton.addEventListener('click', (event) => {
+      event.target.parentNode.remove(); 
+    })
+    card.append(deleteButton);
 
     mainDiv.append(card);
 
@@ -132,7 +127,7 @@ myLibrary.forEach(book => {
     displayBook(book);
 })
 
-//add sort by title (a-z), (z-a), pages, dateAdded, review
+//add sort by title (a-z), (z-a), pages, dateAdded, rating
 
 const addBooksButton = document.querySelector('.addBooks');
 const closeDialog = document.querySelector('.close');
@@ -145,3 +140,21 @@ addBooksButton.addEventListener('click', () => {
 closeDialog.addEventListener('click', () => {
   dialog.close();
 })
+
+
+const submitFormButton = document.querySelector('.submit');
+
+submitFormButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const publishDate = document.getElementById('publishDate').value;
+  const coverArt = document.getElementById('coverArt').value;
+  const newBook = new Book(title, author, pages, publishDate, coverArt);
+  console.log(newBook);
+  displayBook(newBook);
+  addBookToLibrary(newBook);
+  dialog.close();
+})
+
